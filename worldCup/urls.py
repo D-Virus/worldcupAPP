d522f8ad50a131ec79d433accf9db36683f195ac
D-Virus <webmaster@hotfixed.net~>
2014-03-25 14:24:00 -0500

    Added URLS and Views

--- worldCup/urls.py
+++ worldCup/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+import equipos.views
+ 
 admin.autodiscover()
-
+ 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'worldCup.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', equipos.views.ListarContinentes.as_view(), name='lista-continentes',),
+  	
     url(r'^admin/', include(admin.site.urls)),
 )
