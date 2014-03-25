c113899e43679e0e26da13eb01fc77f1a02b89d7
D-Virus <webmaster@hotfixed.net~>
2014-03-25 14:22:11 -0500

    Added URLS and Views

--- equipos/views.py
+++ equipos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from models import Continente
 # Create your views here.
+ 
+ 
+class ListarContinentes(ListView):
+ 
+    model = Continente
+    template_name = 'lista_continentes.html'
